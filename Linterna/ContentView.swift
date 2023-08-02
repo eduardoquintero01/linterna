e1282723ed7f00de22ef69daf5da209ce148266f
newN <newN@gmil.com>
2023-08-02 17:04:11 -0600

    first commit

---
+++ Linterna/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Linterna
+//
+//  Created by Alumno on 02/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var  lamp = false
+    var body: some View {
+        VStack{
+            ZStack{
+                Color(lamp ? .black : .white)
+                Button(action:{
+                    
+                    lamp.toggle()
+                    
+                }){
+                    Text("encender")
+                }
+            }
+        }.edgesIgnoringSafeArea(.all)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Linterna/LinternaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinternaApp.swift
+//  Linterna
+//
+//  Created by Alumno on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LinternaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
